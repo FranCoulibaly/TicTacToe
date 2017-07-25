2dfdaa604c9579a1ce8c9d9c33811465f54280be
Francesca Coulibaly <fran.coulibaly@gmail.com>
2017-07-25 16:14:07 +0200

    added player prompts

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,8 @@
 const Prompt = require("syncprompt");
-var game = [];
+var game = [[0, 0, 0],
+			[0, 0, 0],
+			[0, 0, 0]];
+var move = Prompt("Player " + this.playerTurn + ", make your move.")
 
 class Player {
 	constructor(){
@@ -16,27 +19,47 @@ class Board{
 	}
 
 	_checkPlayerTurn(){
-		if (this.playerTurn === "X") {
-			this.playerTurn = "O";
-			Prompt("Player O, make your move.")
+		
+		if (this.numOfMoves < 8) {
+			if (this.playerTurn === "X") {
+				this.playerTurn = "O";
+			}
+			else if (this.playerTurn === "O") {
+				this.playerTurn = "X";
+			}
+			console.log(move);
+			this.numOfMoves++;
+			this._checkPlayerTurn();
 		}
-		else if (this.playerTurn === "O") {
-			this.playerTurn = "X";
-			Prompt("Player X, make your move.")
+		else {
+		this.state = "Game over";
+		console.log(this.state);
+		process.exit();
 		}
+	}
 
+	// _makeMove(){
+	// 	coOrds = parseInt(move);
+	// 	console.log(coOrds);
+
+	// }
+	_emptyBoard(){
+		console.log("	   1	   2	   3");
+		console.log("	_______________________");
+		console.log("	1|	|	|	|");
+		console.log("	_______________________");
+		console.log("	2|	|	|	|");
+		console.log("	_______________________");
+		console.log("	3|	|	|	|");
+	}
+	startGame(){
+		this._emptyBoard();
+		this._checkPlayerTurn();
 	}
 }
 
-function emptyBoard(){
-	console.log("	   1	   2	   3");
-	console.log("	_______________________");
-	console.log("	1|	|	|	|");
-	console.log("	_______________________");
-	console.log("	2|	|	|	|");
-	console.log("	_______________________");
-	console.log("	3|	|	|	|");
-	}
-	
-emptyBoard();
+let board = new Board();
+board.startGame();
+
+
 
