fb15ba3bd46f7b18cd4830436860c344fdb62c52
Francesca Coulibaly <fran.coulibaly@gmail.com>
2017-08-03 17:30:58 +0200

    reads input as coordinates

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,7 +2,6 @@ const Prompt = require("syncprompt");
 var game = [[0, 0, 0],
 			[0, 0, 0],
 			[0, 0, 0]];
-var move = Prompt("Player " + this.playerTurn + ", make your move.")
 
 class Player {
 	constructor(){
@@ -12,14 +11,14 @@ class Player {
 
 class Board{
 	constructor(stateOfPlay) {
-		this.playerTurn = "X";
+		this.playerTurn = "O";
 		this.numOfMoves = 0;
 		this.state = "Game in progress";
 		this.maxNumTurns = 9;
+		this.currentMove = [];
 	}
 
 	_checkPlayerTurn(){
-		
 		if (this.numOfMoves < 8) {
 			if (this.playerTurn === "X") {
 				this.playerTurn = "O";
@@ -27,22 +26,52 @@ class Board{
 			else if (this.playerTurn === "O") {
 				this.playerTurn = "X";
 			}
-			console.log(move);
-			this.numOfMoves++;
-			this._checkPlayerTurn();
+			// console.log(this.playerTurn + " it's your move");
 		}
 		else {
 		this.state = "Game over";
 		console.log(this.state);
-		process.exit();
+
+		return;
+		};
+	}
+
+	_printPlayerMove(a, b){
+		if (this.player === "O") {
+			console.log(Prompt("Player O it's your turn"));
+
+			// let register = parseInt(playO);
+			// console.log(register);
+		}
+		else {
+			let coordinates = [];
+			let	input = Prompt("Player X it's your turn");
+			let output = input.split(" ");
+			coordinates = [parseInt(output[0]), parseInt(output[1])];
+			game[coordinates[0]][coordinates[1]] = "X";
+			console.log(game);
+
+			// let register = parseInt(playX);
+			// console.log(register);
 		}
 	}
 
-	// _makeMove(){
-	// 	coOrds = parseInt(move);
-	// 	console.log(coOrds);
+	// _checkMove(){
+	// 	if (currentMove ==){}
 
 	// }
+
+	// _playingBoard(){
+	// 	console.log("	   1	   2	   3");
+	// 	console.log("	_______________________");
+	// 	console.log("	1|" + game[0][0] +	"|" + game[0][1] +	"|" + game[0][2]	"|");
+	// 	console.log("	_______________________");
+	// 	console.log("	2|"	+ game[1][0] +  "|" + game[1][1] +	"|" + game[1][2] +	"|");
+	// 	console.log("	_______________________");
+	// 	console.log("	3|" + game[2][0] +	"|" + game[2][1] +	"|" + game[2][2] +	"|");
+	// }
+
+
 	_emptyBoard(){
 		console.log("	   1	   2	   3");
 		console.log("	_______________________");
@@ -52,9 +81,11 @@ class Board{
 		console.log("	_______________________");
 		console.log("	3|	|	|	|");
 	}
+
 	startGame(){
 		this._emptyBoard();
 		this._checkPlayerTurn();
+		this._printPlayerMove();
 	}
 }
 
