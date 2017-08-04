000ccdb46a3c72577da1aea3b28140f9950e9df0
Francesca Coulibaly <fran.coulibaly@gmail.com>
2017-08-04 11:11:15 +0200

    game primarily functional

--- TicTacToe.js
+++ TicTacToe.js
@@ -12,87 +12,113 @@ class Player {
 class Board{
 	constructor(stateOfPlay) {
 		this.playerTurn = "X";
-		this.numOfMoves = 0;
+		this.numOfMoves = -1;
 		this.state = "Game in progress";
 		this.maxNumTurns = 9;
 		this.currentMove = [];
 	}
 
 	_checkPlayerTurn(){
-		if (this.numOfMoves < 8) {
-			if (this.playerTurn === "X") {
-				this.playerTurn = "O";
-				this.numOfMoves++;
-				this._printPlayerMove();
-			}
-			else if (this.playerTurn === "O") {
-				this.playerTurn = "X";
-				this.numOfMoves++;
-				this._printPlayerMove();
-			}
-			// console.log(this.playerTurn + " it's your move");
-		}
-		else {
-			return "It's a draw, game over!";
-		};
+		if (this.playerTurn === "X") {
+			this.playerTurn = "O";
 
+		}
+		else if (this.playerTurn === "O") {
+			this.playerTurn = "X";
+		}
+		this.numOfMoves++;
+		// console.log(this.numOfMoves);
+		this._printPlayerMove();
+		// console.log(this.playerTurn + " it's your move");
+	
+
+// Not currently working		
+		// console.log("Game over!")
+		// return;
+	}
+
+	_checkWinner(){
+		// rows
+		if ((game[0][0] === "X" && game[0][1] === "X" && game[0][2] === "X") || 
+			(game[1][0] === "X" && game[1][1] === "X" && game[1][2] === "X") ||
+			(game[2][0] === "X" && game[2][1] === "X" && game[2][2] === "X") ||
+			//columns
+			(game[0][0] === "X" && game[1][0] === "X" && game[2][0] === "X") || 
+			(game[0][1] === "X" && game[1][1] === "X" && game[2][1] === "X") ||
+			(game[0][2] === "X" && game[1][2] === "X" && game[2][2] === "X") ||
+			//diagonals
+			(game[0][0] === "X" && game[1][1] === "X" && game[2][2] === "X") || 
+			(game[0][2] === "X" && game[1][1] === "X" && game[2][0] === "X")){
+			console.log("Player X wins!");
+			return;
+		}
+		else if ((game[0][0] === "O" && game[0][1] === "O" && game[0][2] === "O") || 
+			(game[1][0] === "O" && game[1][1] === "O" && game[1][2] === "O") ||
+			(game[2][0] === "O" && game[2][1] === "O" && game[2][2] === "O") ||
+			//columns
+			(game[0][0] === "O" && game[1][0] === "O" && game[2][0] === "O") || 
+			(game[0][1] === "O" && game[1][1] === "O" && game[2][1] === "O") ||
+			(game[0][2] === "O" && game[1][2] === "O" && game[2][2] === "O") ||
+			//diagonals
+			(game[0][0] === "O" && game[1][1] === "O" && game[2][2] === "O") || 
+			(game[0][2] === "O" && game[1][1] === "O" && game[2][0] === "O")){
+			console.log("Player O wins!");
+			return;
+		}
+		else if (this.numOfMoves > 8){
+			console.log("It's a draw!");
+			return;
+		}
+		this._checkPlayerTurn();
 	}
 
 	_printPlayerMove(){
-		let coordinates = [];
-		if (this.playerTurn === "O") {
-			let	input = Prompt("Player O it's your turn");
-			let output = input.split(" ");
-			coordinates = [parseInt(output[0]), parseInt(output[1])];
+		let	input = Prompt("Player " + this.playerTurn + " it's your turn");
+		let output = input.split(" ");
+		let x = parseInt(output[0]);
+		let y = parseInt(output[1]);
 
-			if (game[coordinates[0]][coordinates[1]] === "O" || game[coordinates[0]][coordinates[1]] === "X"){
-				console.log("This space is already taken");
-				this._printPlayerMove();
-			} 
-			else {
-			game[coordinates[0]][coordinates[1]] = "O";
-			// console.log(game);
-			this._playingBoard();
-			}
+// Not currently working
+		if (y === NaN || x === NaN) {
+			console.log("Please give two coordinates");
+			this._printPlayerMove();
 		}
-
-		else if (this.playerTurn === "X"){
-			let	input = Prompt("Player X it's your turn");
-			let output = input.split(" ");
-			coordinates = [parseInt(output[0]), parseInt(output[1])];
-			if (game[coordinates[0]][coordinates[1]] === "O" || game[coordinates[0]][coordinates[1]] === "X"){
-				console.log("This space is already taken");
-				this._printPlayerMove();
-			} 
-			else {
-			game[coordinates[0]][coordinates[1]] = "X";
-			// console.log(game);
-			this._playingBoard();
-			}
+		else if (x > 2 || x < 0 || y > 2 || y < 0) {
+			console.log("This is not a valid move, please enter coordinates between 0 and 2");
+			this._printPlayerMove();
+		}
+		else if (game[x][y] === "O" || game[x][y] === "X"){
+			console.log("This space is already taken");
+			this._printPlayerMove();
+		} 
+		else {
+		game[x][y] = this.playerTurn;
+		// console.log(y);
+		this._playingBoard();
 		}	
 	}
 
 
 	_playingBoard(){
-		console.log("	   1	   2	   3");
+		console.log("	   0	   1	   2");
 		console.log("	_______________________"); 
-		console.log("	1|   " + game[0][0] +	"   |   " + game[0][1] +	"   |   " + game[0][2] +	"  |");
+		console.log("	0|   " + game[0][0] +	"   |   " + game[0][1] +	"   |   " + game[0][2] +	"  |");
 		console.log("	_______________________");
-		console.log("	2|   "	+ game[1][0] +  "   |   " + game[1][1] +	"   |   " + game[1][2] +	"  |");
+		console.log("	1|   "	+ game[1][0] +  "   |   " + game[1][1] +	"   |   " + game[1][2] +	"  |");
 		console.log("	_______________________");
-		console.log("	3|   " + game[2][0] +	"   |   " + game[2][1] +	"   |   " + game[2][2] +	"  |");
-		this._checkPlayerTurn();
+		console.log("	2|   " + game[2][0] +	"   |   " + game[2][1] +	"   |   " + game[2][2] +	"  |");
+		this._checkWinner();
 	}
 
 
 	_emptyBoard(){
-		console.log("	   1	   2	   3");
+		console.log("	   0	   1	   2");
+		console.log("	_______________________");
+		console.log("	0|	|	|	|");
 		console.log("	_______________________");
 		console.log("	1|	|	|	|");
 		console.log("	_______________________");
 		console.log("	2|	|	|	|");
-		console.log("	_______________________");
-		console.log("	3|	|	|	|");
 		this._checkPlayerTurn();
 	}
 
